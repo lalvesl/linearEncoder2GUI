99a415704465bf506fa02ab44101446e6c0d32a8
Lucas Alves <alvesdelima.lucas45@gmail.com>
2023-12-04 00:50:16 -0300

    Criado macros de manipulação de memória.

--- src/tools.c
+++ src/tools.c
@@ -2,3 +2,20 @@
 
 #define MIN(a, b) (((a) < (b)) ? (a) : (b))
 #define MAX(a, b) (((a) > (b)) ? (a) : (b))
+
+// GPIO setup macros. Always use INP_GPIO(x) before using OUT_GPIO(x) or SET_GPIO_ALT(x,y)
+#define INP_GPIO(g) *(gpio + ((g) / 10)) &= ~(7 << (((g) % 10) * 3))
+#define OUT_GPIO(g) *(gpio + ((g) / 10)) |= (1 << (((g) % 10) * 3))
+#define SET_GPIO_ALT(g, a) *(gpio + (((g) / 10))) |= (((a) <= 3 ? (a) + 4 : (a) == 4 ? 3  \
+                                                                                     : 2) \
+                                                      << (((g) % 10) * 3))
+
+#define GPIO_SET *(gpio + 7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR *(gpio + 10) // clears bits which are 1 ignores bits which are 0
+
+#define GET_GPIO(g) (*(gpio + 13) & (1 << g)) // 0 if LOW, (1<<g) if HIGH
+#define GET_STATE(g) (((GET_GPIO(g)) == 0) ? (0) : (1))
+#define INV_STATE(v) (((v) == 0) ? (1) : (0))
+
+#define GPIO_PULL *(gpio + 37)     // Pull up/pull down
+#define GPIO_PULLCLK0 *(gpio + 38) // Pull up/pull down clock
