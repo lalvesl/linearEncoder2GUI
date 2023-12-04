dc9068643c7e67e28d130ad87518527b18f7c339
Lucas Alves <alvesdelima.lucas45@gmail.com>
2023-12-04 01:01:47 -0300

    Revisado dependências de execução;

--- src/binder.h
+++ src/binder.h
@@ -1,15 +1,19 @@
-// #include <stdio.h>
-// #include <malloc.h>
-// #include <pthread.h>
-
 #include <stdio.h>
 
-//necessery for main access and read memory (and registers)
+// necessery for main access and read memory (and registers)
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <unistd.h>
 
+#include <pthread.h>
+
+#include <malloc.h>
+
+#ifndef enver
+#define enver
+#include "enver.h"
+#endif
 
 #ifndef types
 #define types
@@ -17,6 +21,11 @@
 #endif
 
 #ifndef tools
-#define tools 1
+#define tools
 #include "tools.c"
+#endif
+
+#ifndef global_variables
+#define global_variables
+#include "global_variables.c"
 #endif
