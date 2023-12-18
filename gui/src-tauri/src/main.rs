6b9178c6051a4f46fd5f60685df56ca809e9b7f5
Lucas Alves <alvesdelima.lucas45@gmail.com>
2023-12-17 23:42:50 -0300

    Criado 'backend' para integração interface e módulo IO, realizado conversão pulse to mm.

--- gui/src-tauri/src/main.rs
+++ gui/src-tauri/src/main.rs
@@ -1,40 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::fs;
+use std::fs::{self, File};
+use std::io::prelude::*;
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_new_position(data: &str) -> f64 {
+    let mut position_file = fs::File::options()
+        .read(true)
+        .write(false)
+        .open("../../io/mem/io")
+        .expect("Error on open position file");
+    let mut position_string = String::new();
+    position_file
+        .read_to_string(&mut position_string)
+        .expect("Error on read position file");
+    position_string.truncate(19);
+    let position_int = match position_string.parse() {
+        Ok(v) => v,
+        Err(_) => 0.,
+    };
+    return (position_int as f64) / 100.;
 }
 
 fn main() {
     tauri::Builder::default()
-        .setup(|_app| {
-            let entries = match fs::read_dir("./src") {
-                Ok(f) => f,
-                Err(_) => panic!("readDir is broken!"),
-            };
-
-            let fds = entries
-                .map(|f| match f {
-                    Ok(f) => f,
-                    Err(_) => panic!("readDir is broken!"),
-                })
-                .map(|fd| fd.path().display().to_string());
-            println!(
-                "{:?}",
-                fds.reduce(|buff: String, fd: String| buff + "," + &fd)
-                    .unwrap()
-            );
-            // let mut fds = match entries.collect::<Result<Vec<fs::DirEntry>, io::Error>>() {
-            //     Ok(fds) => for fd in fds{ fd.path()},
-            //     Err(_) => panic!("Error on read dir"),
-            // };
-            Ok(())
-        })
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_new_position])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
